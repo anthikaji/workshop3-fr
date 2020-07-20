bb5a9111dd7bd030c1ab1c60770a08dc8a20cd72
Anthika Jirattananon <anthikajir@mathregit.group>
2020-07-20 14:54:21 +0700

    add input name and last name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  msg:string;
+  msgg:string;
+  title = 'Anthika';
+  score = 100;
+  colors = ['Red','Green','Blue'] //array
+  show = true;
+  firstName = '';
+  lastName='';
+  
+
+  inputYourName(event : any ):void {
+ this.firstName = event.target.value;
+  }
+  inputLastName(input : string ):void {
+    this.lastName = input;
+     }
+     clickEvent(){
+       this.msg = this.firstName;
+       this.msgg = this.lastName
+    
+  }
 }
